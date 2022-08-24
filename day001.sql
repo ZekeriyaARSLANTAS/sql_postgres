f7204aa6ff377794ae3367eca18ea8ca48534386
Zekeriya ARSLANTAŞ <zekeriyaarslantas@gmail.com>
2022-08-24 23:33:47 +0300

    ver1

---
+++ day001.sql
@@ -0,0 +1,40 @@
+--Personel isminde bir tablo oluşturalım
+create table personel(
+pers_id int,
+isim varchar(30),
+sehir varchar(30),
+maas int,
+sirket varchar(20),
+adres varchar(50)
+);
+--Varolan personel tablosundan pers_id,sehir,adres fieldlarına sahip personel_adres adında yeni tablo olusturalım
+create table personel_adres
+as
+select pers_id, sehir, adres from personel;
+select * from personel
+-- DML --> Data Manupulation Lang.
+-- INSERT - UPDATE - DELETE
+--Tabloya veri ekleme, tablodan veri güncelleme ve silme işlemlerinde kullanılan komutlar
+--INSERT
+create table student
+(
+id varchar(4),
+st_name varchar(30),
+age int
+);
+INSERT into student VALUES ('1001','Ali Can',25);
+INSERT into student VALUES ('1002','Veli Can',35);
+INSERT into student VALUES ('1003','Ayse Can',45);
+INSERT into student VALUES ('1004','Derya Can',55);
+
+-tabbloya parcalı veri ekleme
+insert into student(st_name,age)values ('Murat Can',65);
+
+--DQL --> DATA QUERY LANG.
+--SELECT
+select * from student;
+select st_name from student;
+
+--SELECT KOMUTU WHERE KOSUL
+select * from student where age>35;
+
---
+++ day3.sql
@@ -0,0 +1,48 @@
+CREATE TABLE calisanlar
+(
+id CHAR(5) PRIMARY KEY, -- not null + unique
+isim VARCHAR(50) UNIQUE, -- UNIQUE --> Bir sutundaki tüm değerlerin BENZERSİZ yani tek olmasını sağlar
+maas int NOT NULL, -- NOT NULL --> Bir sutunun NULL içermemesini yani boş olmamasını sağlar
+ise_baslama DATE
+);
+INSERT INTO calisanlar VALUES('10002', 'Mehmet Yılmaz' ,12000, '2018-04-14');
+INSERT INTO calisanlar VALUES('10008', null, 5000, '2018-04-14');
+--INSERT INTO calisanlar VALUES('10010', Mehmet Yılmaz, 5000, '2018-04-14'); --Unique
+INSERT INTO calisanlar VALUES('10004', 'Veli Han', 5000, '2018-04-14');
+INSERT INTO calisanlar VALUES('10005', 'Mustafa Ali', 5000, '2018-04-14');
+--INSERT INTO calisanlar VALUES('10006', 'Canan Yaş', NULL, '2019-04-12'); --NOT NULL
+INSERT INTO calisanlar VALUES('10003', 'CAN', 5000, '2018-04-14');
+--INSERT INTO calisanlar VALUES('10007', 'CAN', 5000, '2018-04-14'); --UNIQUE
+--INSERT INTO calisanlar VALUES('10009', 'cem', '', '2018-04-14'); --NOT NULL
+INSERT INTO calisanlar VALUES('', 'osman', 2000, '2018-04-14');
+--INSERT INTO calisanlar VALUES('', 'osman can', 2000, '2018-04-14'); --PRIMARY KEY
+--INSERT INTO calisanlar VALUES( '10002', 'ayse Yılmaz' ,12000, '2018-04-14'); --PRIMARY KEY
+--INSERT INTO calisanlar VALUES( null, 'filiz ' ,12000, '2018-04-14'); -- PRIMARY KEY
+-- FOREIGN KEY--
+CREATE TABLE adresler
+(
+adres_id char(5) ,
+sokak varchar(20),
+cadde varchar(30),
+sehir varchar(20),
+CONSTRAINT fk FOREIGN KEY (adres_id) REFERENCES calisanlar(id)   
+);
+INSERT INTO adresler VALUES('10003','Mutlu Sok', '40.Cad.','IST');
+INSERT INTO adresler VALUES('10003','Can Sok', '50.Cad.','Ankara');
+INSERT INTO adresler VALUES('10002','Ağa Sok', '30.Cad.','Antep');
+select * from adresler;
+INSERT INTO adresler VALUES('10012','Ağa Sok', '30.Cad.','Antep'); 
+--Parent tabloda olmayan id ile child tabloya ekleme yapamayız
+INSERT INTO adresler VALUES(NULL,'Ağa Sok', '30.Cad.','Antep');
+--Calısanlar id ile adresler tablosundaki adres_id ile eşlesenlere bakmak için
+select * from calisanlar,adresler WHERE calisanlar.id = adresler.adres_id;
+DROP table calisanlar
+--Parant tabloyu yani primary key olan tabloyu silmek istediğimizde tabloyu silmez
+--Önce child tabloyu silmemiz gerekir
+delete from calisanlar where id = '10002'; --Parent
+delete from adresler WHERE adres_id = '10002'; --Child
+drop table calisanlar;
+-- ON DELETE CASCADE --
+--Her defasında önce child tablodaki verileri silmek yerine 
+--ON DELETE CASCADE silme özelliğini aktif hale getirebiliriz
+--Bunun için FK olan satırın en sonuna ON DELETE CASCADE komutunu yazmamız yeterli
